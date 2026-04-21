1bac6e8db58da82d40aecccc2b80f22b8dc8573a
Jasmir Basheer <jasmirbasheer@Jasmirs-MacBook-Air.local>
2026-04-21 10:20:38 +0530

    updated css design

--- src/api/api.js
+++ src/api/api.js
@@ -2,30 +2,22 @@ import axios from "axios";
 
 const BASE_URL = "https://t4e-testserver.onrender.com/api";
 
-/* 🔐 Get Token */
-export const getToken = async () => {
-  const { data } = await axios.post(
-    `${BASE_URL}/public/token`,
-    {
-      studentId: "E0123008",
-      password: "445291",
-      set: "A"
-    }
-  );
+export const getToken = async (studentId, password, set) => {
+  const { data } = await axios.post(`${BASE_URL}/public/token`, {
+    studentId :"E0123008",
+    password : "445291",
+    set:"A",
+  });
 
-  return data.token;
+  return data;
 };
 
-/* 📊 Get Dataset */
-export const getDataset = async (token) => {
-  const { data } = await axios.get(
-    `${BASE_URL}/private/data`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-  );
+export const getDataset = async (token, dataUrl) => {
+  const { data } = await axios.get(`${BASE_URL}${dataUrl}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
   return data.data;
 };
